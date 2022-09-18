40d4f57126a8a13e250c68a1acfcbada236ecfff
7ganam@gmail.com <7ganam@gmail.com>
2022-09-18 22:30:58 +0200

    added speaker

--- config/database.js
+++ config/database.js
@@ -1,11 +1,43 @@
-const path = require('path');
+// const { parse } = require("pg-connection-string");
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+//   return {
+//     connection: {
+//       client: "postgres",
+//       connection: {
+//         host,
+//         port,
+//         database,
+//         user,
+//         password,
+//       },
+//       debug: false,
+//       useNullAsDefault: true,
+//     },
+//   };
+// };
+const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+const path = require("path");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password, ssl } = parse(
+    env("DATABASE_URL")
+  );
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        },
+      },
+      useNullAsDefault: true,
     },
-    useNullAsDefault: true,
-  },
-});
+  };
+};

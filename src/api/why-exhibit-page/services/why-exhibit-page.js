f4f83c8ded3223e1223715306c097f46de2be7ea
Omar ZIdan <s-omarzidan@zewailcity.edu.eg>
2022-09-28 02:42:08 +0200

    created why exhibit page connection

---
+++ src/api/why-exhibit-page/services/why-exhibit-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * why-exhibit-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::why-exhibit-page.why-exhibit-page');
